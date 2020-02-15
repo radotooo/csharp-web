51141a49952c309a8eb983ff4175302ec9a1d152
Radoto <radotooo@gmail.com>
2020-02-15 21:11:37 +0200

    update
    update

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SulsApp/Controllers/UsersController.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SulsApp/Controllers/UsersController.cs
@@ -27,6 +27,11 @@ namespace SulsApp.Controllers
 
         public HttpResponse Login()
         {
+            if (this.IsUserLoggedIn())
+            {
+                return this.Redirect("/");
+            }
+
             return this.View();
         }
 
@@ -46,6 +51,11 @@ namespace SulsApp.Controllers
 
         public HttpResponse Register()
         {
+            if (this.IsUserLoggedIn())
+            {
+                return this.Error("You are already registered.");
+            }
+
             return this.View();
         }
 
